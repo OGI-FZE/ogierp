3b727be1f8a1c8770052342ab545cb8459b98cd2
rizwanpak <rizwan.shahid@havenir.com>
2021-10-05 12:17:35 +0500

    feat(opportunity): 1-Can create rental estimation from Opportunity

--- oil_and_gas_international/public/js/opportunity.js
+++ oil_and_gas_international/public/js/opportunity.js
@@ -8,5 +8,45 @@ frappe.ui.form.on("Opportunity Item", {
                 ]
             }
         }
+    },
+    
+})
+frappe.ui.form.on("Opportunity", {
+    refresh(frm){
+        if(frm.doc.docstatus==0){
+            console.log('working');
+            custom_buttom(frm)
+        }
     }
 })
+const custom_buttom=(frm)=>{
+    frm.add_custom_button('Rental Estimation', () => {
+        const doc = frm.doc;
+		frappe.run_serially([
+		() => frappe.new_doc('Rental Estimation'),
+		() => {
+			cur_frm.doc.customer = doc.party_name;
+            cur_frm.doc.customer_name = doc.customer_name;
+			cur_frm.doc.date = doc.transaction_date;
+			cur_frm.doc.valid_till = doc.valid_till;
+			cur_frm.doc.opportunity = doc.name;
+			cur_frm.doc.items = []
+			for(let row of doc.items){
+                if(row.item_type=='Rental'){
+                    let new_row = cur_frm.add_child('items', {
+                        'quantity': row.qty,
+                        'item_code':row.item_code,
+                        'item_name':row.item_name,
+                        'description':row.description
+                    })
+                }
+				
+			}
+			
+			cur_frm.refresh()
+		}
+		])
+        
+    }, 'Create');
+    
+}
