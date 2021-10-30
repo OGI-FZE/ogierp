0ba21c9387a701a07b524676241a56a2892b79dd
Faisal Zahoor <faisal-zahoor>
2021-10-30 21:42:43 +0500

    fix: relevant item fetch in asset formation

--- oil_and_gas_international/oil_and_gas_international/doctype/asset_formation/asset_formation.js
+++ oil_and_gas_international/oil_and_gas_international/doctype/asset_formation/asset_formation.js
@@ -3,13 +3,33 @@
 
 frappe.ui.form.on('Asset Formation', {
 	setup: function (frm) {
-		frm.events.set_item_queries(frm);
+		set_query(frm);
 	},
 
 	refresh: function (frm) {
 		frappe.breadcrumbs.add('Assets', 'Asset Formation')
-		frm.events.set_item_queries(frm);
-		frm.events.show_create_asset_button(frm);
+		create_custom_buttons(frm);
+	},
+
+	asset_item_code: function (frm) {
+		const asset_item_code = frm.doc.asset_item_code
+		const cdt = frm.doc.doctype
+		const cdn = frm.doc.name
+
+		frappe.db.get_value("Item", asset_item_code, "relevant_item").then(r => {
+			if (r.message.relevant_item) {
+				frappe.model.set_value(cdt, cdn, "item_code", r.message.relevant_item)
+			}
+		})
+	},
+
+	default_location(frm) {
+		for (const row of frm.doc.items) {
+			if (!row.location) {
+				row.location = frm.doc.default_location
+			}
+		}
+		frm.refresh()
 	},
 
 	item_code: function (frm) {
@@ -23,84 +43,82 @@ frappe.ui.form.on('Asset Formation', {
 	},
 
 	fetch_serial_no: function (frm) {
-		frm.events.show_serial_dialog(frm);
+		show_serial_dialog(frm);
 	},
-
-	show_create_asset_button: function (frm) {
-		if (frm.doc.docstatus == 1 && frm.doc.status == 'Submitted') {
-			frm.add_custom_button('Convert to Assets', () => {
-				frm.call('create_assets')
-					.then(r => {
-						frm.reload_doc();
-						frappe.msgprint('Asset Created in Draft Mode')
-					})
-			})
-		}
-	},
-
-	set_item_queries: function (frm) {
-		// Stock Item Query
-		frm.set_query('item_code', () => {
-			return {
-				filters: {
-					is_fixed_asset: 0,
-					is_stock_item: 1,
-					has_serial_no: 1
-				}
-			}
-		})
-
-		// Asset Item Query
-		frm.set_query('asset_item_code', () => {
-			return {
-				filters: {
-					is_fixed_asset: 1
-				}
-			}
-		})
-	},
-
-	show_serial_dialog: function (frm) {
-		if (frm.doc.item_code && frm.doc.warehouse) {
-			new frappe.ui.form.MultiSelectDialog({
-				doctype: "Serial No",
-				target: frm,
-				setters: {
-					item_code: frm.doc.item_code,
-				},
-				date_field: "purchase_date",
-				get_query() {
-					return {
-						filters: {
-							docstatus: ['!=', 2],
-							status: 'Active',
-							company: frm.doc.company,
-							warehouse: frm.doc.warehouse
-						}
-					}
-				},
-				action(selections) {
-					frm.doc.items = [];
-					for (let i in selections) {
-						frm.add_child('items', {
-							item_code: frm.doc.item_code,
-							serial_no: selections[i]
-						});
-					}
-					frm.refresh_field('items');
-					cur_dialog.hide();
-				}
-			});
-		} else {
-			frappe.msgprint('Select Item and Warehouse');
-		}
-	},
-	default_location(frm) {
-		for (const row of frm.doc.items) {
-			if (!row.location) {
-				row.location = frm.doc.default_location
-			}
-		}
-		frm.refresh()
-	}
 });
+
+// functions
+const set_query = (frm) => {
+	// Stock Item Query
+	frm.set_query('item_code', () => {
+		return {
+			filters: {
+				is_fixed_asset: 0,
+				is_stock_item: 1,
+				has_serial_no: 1
+			}
+		}
+	})
+
+	// Asset Item Query
+	frm.set_query('asset_item_code', () => {
+		return {
+			filters: {
+				is_fixed_asset: 1
+			}
+		}
+	})
+}
+
+const create_custom_buttons = (frm) => {
+	if (frm.doc.docstatus == 1 && frm.doc.status == 'Submitted') {
+		create_asset_button(frm)
+	}
+}
+
+const create_asset_button = (frm) => {
+	frm.add_custom_button('Convert to Assets', () => {
+		frm.call('create_assets')
+			.then(r => {
+				frm.reload_doc();
+				frappe.msgprint('Asset Created in Draft Mode')
+			})
+	})
+
+}
+
+const show_serial_dialog = (frm) => {
+	if (frm.doc.item_code && frm.doc.warehouse) {
+		new frappe.ui.form.MultiSelectDialog({
+			doctype: "Serial No",
+			target: frm,
+			setters: {
+				item_code: frm.doc.item_code,
+			},
+			date_field: "purchase_date",
+			get_query() {
+				return {
+					filters: {
+						docstatus: ['!=', 2],
+						status: 'Active',
+						company: frm.doc.company,
+						warehouse: frm.doc.warehouse
+					}
+				}
+			},
+			action(selections) {
+				frm.doc.items = [];
+				for (let i in selections) {
+					frm.add_child('items', {
+						item_code: frm.doc.item_code,
+						serial_no: selections[i]
+					});
+				}
+				frm.refresh_field('items');
+				cur_dialog.hide();
+			}
+		});
+	} else {
+		frappe.msgprint('Select Item and Warehouse');
+	}
+}
