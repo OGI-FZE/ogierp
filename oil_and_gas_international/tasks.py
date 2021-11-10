76637dd8d748a3610c232ce34db167f46d4da558
Faisal Zahoor <faisal-zahoor>
2021-11-10 12:50:13 +0500

    fix: multiple rental orders

--- oil_and_gas_international/tasks.py
+++ oil_and_gas_international/tasks.py
@@ -13,7 +13,7 @@ def daily():
 
 def make_rental_timesheet():
     rental_order_items = frappe.get_list("Rental Order Item", {
-        "status": ["!=", "On Hold"],
+        "on_hold": 0,
         "from_date": ["<=", today()],
         "to_date": [">=", today()]
     }, ["parent"])
@@ -23,6 +23,7 @@ def make_rental_timesheet():
         rental_orders.append(row.parent)
 
     rental_orders = Counter(rental_orders)
+
     for row in rental_orders:
         rental_order = frappe.get_doc("Rental Order", row)
         ro_exists = frappe.db.exists({
@@ -41,31 +42,29 @@ def make_rental_timesheet():
 
             has_items = False
             for item in rental_order.items:
-                if item.status != "on hold":
-                    rental_assets = frappe.get_list(
-                        "Rental Issue Note Item", {"rental_order_item": item.name}, ["assets"])
-                    qty = 0
-                    for row in rental_assets:
-                        assets = row.assets
-                        assets = assets.split("\n")
-                        for asset in assets:
-                            if asset:
-                                rental_status = frappe.get_value(
-                                    "Asset", asset, "rental_status")
-                                if rental_status == "In Use":
-                                    qty = qty + 1
+                rental_assets = frappe.get_list("Rental Issue Note Item", {
+                                                "rental_order_item": item.name}, ["assets"])
 
-                    if qty:
-                        rental_timesheet.append("items", {
-                            "item_code": item.item_code,
-                            "qty": qty,
-                            "rate": item.rate,
-                            "asset_location": item.asset_location,
-                        })
-                        has_items = True
+                qty = 0
+                for row in rental_assets:
+                    assets = row.assets
+                    assets = assets.split("\n")
+                    for asset in assets:
+                        if asset:
+                            rental_status = frappe.get_value(
+                                "Asset", asset, "rental_status")
+                            if rental_status == "In Use":
+                                qty = qty + 1
+                if qty:
+                    rental_timesheet.append("items", {
+                        "item_code": item.item_code,
+                        "qty": qty,
+                        "rate": item.rate,
+                        "asset_location": item.asset_location,
+                    })
+                    has_items = True
 
             if has_items:
                 rental_timesheet.save()
                 rental_timesheet.submit()
-
-    frappe.db.commit()
+                frappe.db.commit()
