3f9ba05fa732feecce9aa2ab39edbf046ca587a5
rizwanpak <rizwan.shahid@havenir.com>
2022-01-20 11:36:33 +0500

    updates(Rental Division Settings, Rental Estimation,Rental Quotation,Rental Order, Rental Timesheet and Sales Invoice): 1- Five status fields have been added in these doctypes and the rates will be fetched from price list. 2- rental timesheet will calculate the amount and can create sales invoice from rental time sheet. 3- if we select timesheet in sales invoice it will fetch all the items used in that time sheet. 4- if rate is not fetched properly in sales invoice, a button is added in the child table to fetch amount of the timesheet

--- oil_and_gas_international/events/shared.py
+++ oil_and_gas_international/events/shared.py
@@ -6,21 +6,42 @@ def get_lost_and_damage_prices(item_code=None):
     if not item_code:
         return 0, 0
 
-    lost_in_hole_price = 0
-    damage_beyond_repair_price = 0
+    lihdbr = 0
+    oprunning = 0
+    standby = 0
+    redress = 0
+    straight = 0
 
     rd_settings = frappe.get_single("Rental Division Settings")
 
-    if rd_settings.lost_in_hole_price:
-        lost_in_hole_price = frappe.get_value("Item Price", {
-            "price_list": rd_settings.lost_in_hole_price,
+    if rd_settings.lih_dbr:
+        lihdbr = frappe.get_value("Item Price", {
+            "price_list": rd_settings.lih_dbr,
             "item_code": item_code
         }, "price_list_rate")
 
-    if rd_settings.damage_beyond_repair_price:
-        damage_beyond_repair_price = frappe.get_value("Item Price", {
-            "price_list": rd_settings.damage_beyond_repair_price,
+    if rd_settings.operational_running:
+        oprunning = frappe.get_value("Item Price", {
+            "price_list": rd_settings.operational_running,
             "item_code": item_code
         }, "price_list_rate")
 
-    return lost_in_hole_price, damage_beyond_repair_price
+    if rd_settings.standby:
+        standby = frappe.get_value("Item Price", {
+            "price_list": rd_settings.standby,
+            "item_code": item_code
+        }, "price_list_rate")
+
+    if rd_settings.redress:
+        redress = frappe.get_value("Item Price", {
+            "price_list": rd_settings.redress,
+            "item_code": item_code
+        }, "price_list_rate")
+
+    if rd_settings.straight:
+        straight = frappe.get_value("Item Price", {
+            "price_list": rd_settings.straight,
+            "item_code": item_code
+        }, "price_list_rate")
+
+    return oprunning, standby , lihdbr, redress, straight
--- oil_and_gas_international/oil_and_gas_international/doctype/rental_timesheet/rental_timesheet.js
+++ oil_and_gas_international/oil_and_gas_international/doctype/rental_timesheet/rental_timesheet.js
@@ -9,6 +9,9 @@ frappe.ui.form.on('Rental Timesheet', {
 		if(frm.is_new()){
 			frappe.model.set_value('Rental Timesheet',frm.doc.name,'status','Draft')
 		}
+		else if(frm.doc.docstatus==1){
+			add_sales_invoice()
+		}
 	}
 });
 
@@ -26,10 +29,14 @@ const get_items_from_rental_order = (frm, cdt, cdn) => {
 				if (!row.on_hold) {
 					const new_row = frm.add_child('items', {
 						'qty': row.qty,
-						'rate': row.rate,
 						'asset_location': row.asset_location,
 						'rental_order': rental_order,
 						'rental_order_item': row.name,
+						'operational_running':row.operational_running,
+						'standby':row.standby,
+						'lihdbr':row.lihdbr,
+						'redress':row.redress,
+						'straight':row.straight,
 					})
 
 					const cdt = new_row.doctype
@@ -46,8 +53,96 @@ const get_items_from_rental_order = (frm, cdt, cdn) => {
 frappe.ui.form.on('Rental Timesheet Item', {
 	get_assets(frm, cdt, cdn) {
 		get_assets_to_receive(frm, cdt, cdn)
+	},
+	operational_running_days(frm,cdt,cdn){
+		let row=locals[cdt][cdn]
+		if(row.operational_running_days){
+			calculate_amount(frm,cdt,cdn)
+		}
+	},
+	standby_days(frm,cdt,cdn){
+		let row=locals[cdt][cdn]
+			if(row.standby_days){
+				console.log('working');
+				calculate_amount(frm,cdt,cdn)
+			}
+	},
+	redress_days(frm,cdt,cdn){
+		let row=locals[cdt][cdn]
+		if(row.redress_days){
+			calculate_amount(frm,cdt,cdn)
+		}
+	},
+	lihdbr_days(frm,cdt,cdn){
+		let row=locals[cdt][cdn]
+		if(row.lihdbr_days){
+			calculate_amount(frm,cdt,cdn)
+		}
+	},
+	straight_days(frm,cdt,cdn){
+		let row=locals[cdt][cdn]
+		if(row.straight_days){
+			calculate_amount(frm,cdt,cdn)
+		}
+	},
+	operational_running(frm,cdt,cdn){
+		let row=locals[cdt][cdn]
+		if(row.operational_running){
+			calculate_amount(frm,cdt,cdn)
+		}
+	},
+	standby(frm,cdt,cdn){
+		let row=locals[cdt][cdn]
+		if(row.standby){
+			calculate_amount(frm,cdt,cdn)
+		}
+	},
+	redress(frm,cdt,cdn){
+		let row=locals[cdt][cdn]
+		if(row.redress){
+			calculate_amount(frm,cdt,cdn)
+		}
+	},
+	lihdbr(frm,cdt,cdn){
+		let row=locals[cdt][cdn]
+		if(row.lihdbr){
+			calculate_amount(frm,cdt,cdn)
+		}
+	},
+	straight(frm,cdt,cdn){
+		let row=locals[cdt][cdn]
+		if(row.straight){
+			calculate_amount(frm,cdt,cdn)
+		}
 	}
 });
+
+const calculate_amount=(frm,cdt,cdn)=>{
+	let row= locals[cdt][cdn]
+	let total=0
+	if(row.operational_running_days && row.operational_running>0){
+		total = row.operational_running_days*row.operational_running
+		console.log(total);
+	}
+	if(row.standby_days && row.standby>0){
+		total = total +( row.standby_days*row.standby)
+		console.log(total);
+	}
+	if(row.redress_days && row.redress>0){
+		total = total+ ( row.redress_days*row.redress)
+		console.log(total);
+	}
+	if(row.lihdbr_days && row.lihdbr>0){
+		total = total+ ( row.lihdbr_days*row.lihdbr)
+		console.log(total);
+	}
+	if(row.straight_days && row.straight>0){
+		total = total+ ( row.straight_days*row.straight)
+		console.log(total);
+	}
+	frappe.model.set_value(cdt,cdn,'amount',total)
+
+}
 
 const get_assets_to_receive = (frm, cdt, cdn) => {
 	const doctype = "Asset"
@@ -78,5 +173,40 @@ const get_assets_to_receive = (frm, cdt, cdn) => {
 			cur_dialog.hide()
 		}
 	});
+}
+
+const add_sales_invoice = () => {
+	cur_frm.add_custom_button('Sales Invoice', () => {
+		const doc = cur_frm.doc
+		frappe.run_serially([
+			() => frappe.new_doc('Sales Invoice'),
+			() => {
+				const cur_doc = cur_frm.doc
+				cur_doc.customer = doc.customer
+				cur_doc.rental_timesheet = doc.name
+				cur_doc.rental_order = doc.rental_order
+				cur_doc.items = []
+
+				for (const row of doc.items) {
+					const new_row = cur_frm.add_child("items", {
+						qty: 1,
+						asset_item:row.item_code,
+						rental_order:row.rental_order,
+						rental_order_item:row.rental_order_item,
+					})
+					const cdt = new_row.doctype
+					const cdn = new_row.name
+					frappe.model.set_value(cdt, cdn, "item_code",'Asset Rent Item')
+					setTimeout(function(){
+						frappe.model.set_value(cdt, cdn, "price_list_rate",row.amount)
+						frappe.model.set_value(cdt, cdn, "rate",row.amount)
+						frappe.model.set_value(cdt, cdn, "amount",row.amount)
+					}, 2000);
+				}
+
+				cur_frm.refresh()
+			}
+		])
+	}, 'Create')
 }
 
