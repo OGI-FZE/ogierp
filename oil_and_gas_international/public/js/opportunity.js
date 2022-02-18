c0435098956ac58b1e7ff5d9f20dd523c71e3bcc
Craftint <shajeer@craftinteractive.ae>
2022-02-18 16:08:04 +0400

    opp to quot

--- oil_and_gas_international/public/js/opportunity.js
+++ oil_and_gas_international/public/js/opportunity.js
@@ -29,6 +29,7 @@ frappe.ui.form.on("Opportunity Item", {
 
 const create_custom_buttons = (frm) => {
     create_rental_estimation(frm)
+    create_rental_quotation(frm)
 }
 
 const create_rental_estimation = (frm) => {
@@ -51,6 +52,37 @@ const create_rental_estimation = (frm) => {
                             'item_name': row.item_name,
                             'description': row.description,
                             'opportunity': doc.name,
+                        })
+                    }
+
+                }
+
+                cur_frm.refresh()
+            }
+        ])
+    }, 'Create');
+}
+
+const create_rental_quotation = (frm) => {
+    frm.add_custom_button('Rental Quotation', () => {
+        const doc = frm.doc;
+        frappe.run_serially([
+            () => frappe.new_doc('Rental Quotation'),
+            () => {
+                cur_frm.doc.customer = doc.party_name;
+                cur_frm.doc.customer_name = doc.customer_name;
+                cur_frm.doc.date = doc.posting_date;
+                cur_frm.doc.valid_till = doc.expected_closing;
+                // cur_frm.doc.opportunity = doc.name;
+                cur_frm.doc.items = []
+                for (let row of doc.items) {
+                    if (row.item_type == 'Rental') {
+                        cur_frm.add_child('items', {
+                            'qty': row.qty,
+                            'item_code': row.item_code,
+                            'item_name': row.item_name,
+                            'description': row.description,
+                            // 'opportunity': doc.name,
                         })
                     }
 
