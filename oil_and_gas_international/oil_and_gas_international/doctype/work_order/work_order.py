e042fa62e5b8b7b6dd5ede9801c32cce9fd1bc9b
Craftint <shajeer@craftinteractive.ae>
2022-03-30 14:49:41 +0400

    work order non related to assets

--- oil_and_gas_international/oil_and_gas_international/doctype/work_order/work_order.py
+++ oil_and_gas_international/oil_and_gas_international/doctype/work_order/work_order.py
@@ -9,34 +9,38 @@ class Work_Order(Document):
 	def on_submit(self):
 		for row in self.items:
 			assets = row.assets
-			assets = assets.split("\n")
-			for asset in assets:
-				if asset:
-					# rental_status = frappe.get_value(
-					#     "Asset", asset, "rental_status")
-					frappe.db.set_value("Asset", asset, "rental_status", "Available for Rent")
+			if assets:
+				assets = assets.split("\n")
+				for asset in assets:
+					if asset:
+						# rental_status = frappe.get_value(
+						#     "Asset", asset, "rental_status")
+						frappe.db.set_value("Asset", asset, "rental_status", "Available for Rent")
 
 	def validate(self):
 		if self.docstatus == 0:
 			for row in self.items:
 				assets = row.assets
-				assets = assets.split("\n")
-				for asset in assets:
-					if asset:
-						frappe.db.set_value("Asset", asset, "rental_status", "On hold for Inspection")
+				if assets:
+					assets = assets.split("\n")
+					for asset in assets:
+						if asset:
+							frappe.db.set_value("Asset", asset, "rental_status", "On hold for Inspection")
 
 	def on_cancel(self):
 		for row in self.items:
 			assets = row.assets
-			assets = assets.split("\n")
-			for asset in assets:
-				if asset:
-					frappe.db.set_value("Asset", asset, "rental_status", "Available for Rent")
+			if assets:
+				assets = assets.split("\n")
+				for asset in assets:
+					if asset:
+						frappe.db.set_value("Asset", asset, "rental_status", "Available for Rent")
 
 	def on_trash(self):
 		for row in self.items:
 			assets = row.assets
-			assets = assets.split("\n")
-			for asset in assets:
-				if asset:
-					frappe.db.set_value("Asset", asset, "rental_status", "Available for Rent")
+			if assets:
+				assets = assets.split("\n")
+				for asset in assets:
+					if asset:
+						frappe.db.set_value("Asset", asset, "rental_status", "Available for Rent")
