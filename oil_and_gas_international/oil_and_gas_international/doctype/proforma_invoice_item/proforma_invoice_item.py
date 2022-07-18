de149670d51adfb9d0147cba9e0f500e5f246a57
RoshnaVijayan <RoshnaVijayan>
2022-07-18 11:11:25 +0400

    Changes to Proforma Invoice

--- oil_and_gas_international/oil_and_gas_international/doctype/proforma_invoice_item/proforma_invoice_item.py
+++ oil_and_gas_international/oil_and_gas_international/doctype/proforma_invoice_item/proforma_invoice_item.py
@@ -1,8 +1,14 @@
-# Copyright (c) 2022, Havenir Solutions and contributors
-# For license information, please see license.txt
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
 
-# import frappe
+
+import frappe
 from frappe.model.document import Document
+
 
 class ProformaInvoiceItem(Document):
 	pass
+
+
+def on_doctype_update():
+	frappe.db.add_index("Proforma Invoice Item", ["item_code", "warehouse"])
