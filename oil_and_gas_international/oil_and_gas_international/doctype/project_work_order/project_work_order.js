7cdecd396137d8462fb032dbaa96213def73af65
riane <riane@craftinteractive.ae>
2023-01-31 13:29:29 +0400

    conditions

--- oil_and_gas_international/oil_and_gas_international/doctype/project_work_order/project_work_order.js
+++ oil_and_gas_international/oil_and_gas_international/doctype/project_work_order/project_work_order.js
@@ -52,7 +52,11 @@ const create_inspection = (frm) => {
 								'item_category': row.item_category,
 								'warehouse': doc.warehouse,
 								'qty':row.qty_in_warehouse,
-								'project': doc.project_reference})
+								'project': doc.project_reference,
+								'project_wo': doc.name,
+								'sales_order': doc.sales_order,
+								'rental_order': doc.rental_order,
+								})
 							}
 						}
 				
@@ -63,7 +67,11 @@ const create_inspection = (frm) => {
 								'item_category': row.item_category,
 								'warehouse': doc.warehouse,
 								'qty':row.qty_in_warehouse,
-								'project': doc.project_reference})
+								'project': doc.project_reference,
+								'project_wo': doc.name,
+								'sales_order': doc.sales_order,
+								'rental_order': doc.rental_order,
+								})
 							}
 						}
 					const fields =	[{
@@ -74,8 +82,14 @@ const create_inspection = (frm) => {
 						fields: [{fieldtype: 'Link',fieldname: 'item_code',label: __('Item code'),options:'Item',in_list_view: 1}, 
 								{fieldtype: 'Data',fieldname: 'item_category',label: __('Item category'),in_list_view: 1},
 								{fieldtype: 'Link',fieldname: 'warehouse',label: __('Warehouse'),options:'Warehouse',in_list_view: 1},
-								{fieldtype: 'Link',fieldname: 'project',label: __('Project'),options:'Project',in_list_view: 1},
-								{fieldtype: 'Float',fieldname: 'qty',label: __('Quantity'),in_list_view: 1}],
+								{fieldtype: 'Link',fieldname: 'project',label: __('Project'),options:'Project',in_list_view: 0},
+								{fieldtype: 'Column Break',fieldname: 'col_bre',label: __(''),in_list_view: 0},
+								{fieldtype: 'Float',fieldname: 'qty',label: __('Quantity'),in_list_view: 1,read_only:1},
+								{fieldtype: 'Link',fieldname: 'project_wo',label: __('Project WO'),options:'Project Work Order',in_list_view: 1},
+								{fieldtype: 'Link',fieldname: 'sales_order',label: __('Sales Order'),options:'Sales Order',in_list_view: 0},
+								{fieldtype: 'Link',fieldname: 'rental_order',label: __('Rental Order'),options:'Rental Order',in_list_view: 0},
+
+					],
 
 						data: data,
 						get_data: () => {
@@ -86,7 +100,7 @@ const create_inspection = (frm) => {
 					console.log(data.length)
 					
 					let table = new frappe.ui.Dialog({
-						title: 'Enter details',
+						title: 'Select Items to Create WO Inspection',
 						fields: fields,
 						primary_action_label: 'Create Inspection',
 						primary_action: function() {
@@ -99,13 +113,18 @@ const create_inspection = (frm) => {
 										"item_code" : idx.item_code,
 										"warehouse": idx.warehouse,
 										"item_category": idx.item_category,
+										"project": idx.project,
+										"project_wo": idx.project_wo,
+										"sales_o": idx.sales_order,
+										"rental_o": idx.rental_order,
+										"qty": idx.qty 
 									},
 									freeze: true,
 									callback: function(r) {
 										if(r.message) {
 												frappe.msgprint({
-												title: __('Contract created'),
-												message: __('Contract generated for the unit selected.'),
+												title: __('Work Order Inspection created'),
+												message: __('Work Order Inspection generated for the Item selected.'),
 												indicator: 'green'
 											});
 										}
