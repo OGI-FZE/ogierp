8dfa73047fb367accfea472d3ec38631b0db651a
Anjusasikumar <anjumols1996@gmail.com>
2023-07-28 16:13:40 +0530

    fix:Stock Forecasting doctype creation,Stock Planning report

---
+++ oil_and_gas_international/oil_and_gas_international/report/stock_planning/stock_planning.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2023, Havenir Solutions and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Stock Planning"] = {
+	"filters": [
+		{
+			'fieldname':'company',
+			'label':__('Company'),
+			'fieldtype':'Link',
+			'options':'Company',
+			'width':100,
+		},
+		{
+			'fieldname':'from_date',
+			'label':__('From Date'),
+			'fieldtype':'Date',
+			'width':100,
+		},
+		{
+			'fieldname':'to_date',
+			'label':__('To Date'),
+			'fieldtype':'Date',
+			'width':100,
+		},
+	]
+};
---
+++ oil_and_gas_international/oil_and_gas_international/report/stock_planning/stock_planning.py
@@ -0,0 +1,157 @@
+# Copyright (c) 2023, Havenir Solutions and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+
+
+
+def execute(filters=None):
+	columns, data = [], []
+	columns = get_columns(filters)
+	data = get_data(filters)
+	return columns, data
+
+
+def get_data(filters):
+	query = f"""
+		SELECT
+			item_type as description,
+			item_group as item_group,
+			quantity as plan_qty,
+			uom as unit,
+			rate as rate,
+			total as total,
+			company as company,
+			posting_date as date
+		FROM
+			`tabStock Forecasting` as sf
+		WHERE
+			forcast_type = "Item"
+		"""
+
+	if filters.company:
+		query = f"{query} AND company={frappe.db.escape(filters.company)}'"
+
+	if filters.from_date and filters.to_date:
+		query = f"{query} AND posting_date BETWEEN '{filters.from_date}' AND '{filters.to_date}'"
+
+	sql_data = frappe.db.sql(query, as_dict=True)
+	
+	item_group_query = f"""
+		SELECT
+			item_type as description,
+			item_group as item_group,
+			quantity as plan_qty,
+			uom as unit,
+			rate as rate,
+			total as total,
+			company as company,
+			posting_date as date
+		FROM
+			`tabStock Forecasting` as sf
+		WHERE
+			forcast_type = "Item Group"
+		"""
+
+	if filters.company:
+		query = f"{item_group_query} AND company={frappe.db.escape(filters.company)}'"
+
+	if filters.from_date and filters.to_date:
+		query = f"{query} AND posting_date BETWEEN '{filters.from_date}' AND '{filters.to_date}'"
+
+	item_group_data = frappe.db.sql(item_group_query, as_dict=True)
+
+	data = {}
+	for i in sql_data:
+		data.setdefault(i['item_group'], []).append(i)
+
+	result = []
+	if data:
+		for j in data:
+			for item in data[j]:
+				item_code = item['description']
+				company = item['company']
+				stock_quantity = get_quantity(item_code, company)
+				item.update({"stock_qty": stock_quantity[0].get("actual_qty"), "company": company})		
+		for d in data:
+			if d:
+				group_row = {}
+				group_row['trunk'] = d or ''
+				group_row['indent'] = 0
+				result.append(group_row)
+				for i in data[d]:
+					i['trunk'] = i['description']
+					i['indent'] = 1
+					result.append(i)
+	for res in result:
+		if res['indent'] == 0:
+			for group in item_group_data:
+				if res['trunk'] == group['description']:
+					res['plan_qty'] = group['plan_qty']
+					res['rate'] = group['rate']
+					res['total'] = group['total']
+	
+	return result
+
+def get_columns(filters):
+	columns = [
+		{
+			'fieldname': 'trunk',
+			'label': _('Description'),
+			'fieldtype': 'Data',
+			'width': 300,
+			'align': 'left'
+		},
+		{
+			'fieldname': 'stock_qty',
+			'label': _('Stock Qty'),
+			'fieldtype': 'Float',
+			'width': 100,
+		},
+		{
+			'fieldname': 'plan_qty',
+			'label': _('Plan Qty'),
+			'fieldtype': 'Float',
+			'width': 100
+		},
+		{
+			'fieldname': 'unit',
+			'label': _('Unit'),
+			'fieldtype': 'Link',
+			'options': 'UOM',
+			'width': 100
+		},
+		{
+			'fieldname': 'rate',
+			'label': _('Rate'),
+			'fieldtype': 'Float',
+			'width': 100
+		},
+		{
+			'fieldname': 'total',
+			'label': _('Total'),
+			'fieldtype': 'Float',
+			'width': 100
+		},
+		
+	]
+	return columns
+
+
+@frappe.whitelist()
+def get_quantity(item_code, company):
+	sql = f"""
+		SELECT
+			sum(ledger.actual_qty) as actual_qty
+   		FROM
+			`tabBin` AS ledger
+   			INNER JOIN `tabItem` AS item    ON ledger.item_code = item.item_code
+   			INNER JOIN `tabWarehouse` as warehouse    ON warehouse.name = ledger.warehouse
+   		WHERE
+			ledger.item_code = "{item_code}" AND
+			warehouse.company = "{company}"
+
+		"""
+	actual_quantity = frappe.db.sql(f"{sql}", as_dict=True)
+	return actual_quantity
