9cf2684876eb816a03aa6e6293dd4e9c730ab91f
rayen <rayenlabdouni@gmail.com>
2024-03-22 11:48:28 +0400

    testing

--- oil_and_gas_international/overriding.py
+++ oil_and_gas_international/overriding.py
@@ -7,12 +7,12 @@ from erpnext.manufacturing.doctype.work_order.work_order import WorkOrder
 from datetime import datetime
 from frappe.utils import date_diff
 
-
+ 
 
 class CustomWorkOrder(WorkOrder):
 
     def validate_sales_order(self):
-        if self.sales_order:
+        if self.sales_order: 
             self.check_sales_order_on_hold_or_close()
             so = frappe.db.sql(
                 """
